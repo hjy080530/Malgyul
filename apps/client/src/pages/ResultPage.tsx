f0d8e92d5975371608436e9d1df8a276fd6ae2cf
hjy080530 <bomaye119@gmail.com>
2025-04-28 00:58:29 +0900

    feat: resultpage 백그라운드 이미지 추가

--- apps/client/src/pages/ResultPage.tsx
+++ apps/client/src/pages/ResultPage.tsx
@@ -1,13 +1,42 @@
 /** @jsxImportSource @emotion/react */
 import Header from '../components/Header.tsx';
+import styled from '@emotion/styled';
+import color from '../types/color.ts';
+import Budda from "../assets/images/Buddasvg.tsx";
 
-
-const ResultPage=()=>{
+const ResultPage = () => {
   return (
-    <>
-      <Header/>
-    </>
-  )
+    <StyledResultPage>
+      <Header />
+      <BuddaWrapper>
+        <Budda width="265%" />
+      </BuddaWrapper>
+    </StyledResultPage>
+  );
 };
+
 export default ResultPage;
 
+const StyledResultPage = styled.div`
+  position: relative; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  padding: 0;
+  background: 
+    linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 100%), ${color.malgyulBlack};
+  gap: 2rem;
+`;
+
+const BuddaWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 32rem;
+  width: fit-content;
+  height: fit-content;
+  z-index: 10; 
+  padding: 0;
+  margin: 0;
+`;
